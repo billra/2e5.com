0c0a42b286d8c2acca7ec3c653c5579085b84305
Bill Ola Rasmussen <billra@gmail.com>
2016-04-17 06:18:01 +0200

    track git operations

--- git/status/index.php
+++ git/status/index.php
@@ -2,11 +2,21 @@
 <html>
 <body>
 
-Local git status:
+<p>local git status:</p>
+
 <pre>
 <?php echo shell_exec("git status 2>&1");
 ?>
 </pre>
+
+<p>website <a href="..">git commands</a></p>
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-701785-1";
+urchinTracker();
+</script>
 
 </body>
 </html>
