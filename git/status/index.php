c6abcf4d3b51420a15b482a28c029fd61a096d78
Bill Ola Rasmussen <billra@gmail.com>
2016-04-17 06:36:44 +0200

    show calling ip address

--- git/status/index.php
+++ git/status/index.php
@@ -2,11 +2,10 @@
 <html>
 <body>
 
-<p>local git status:</p>
+<p><?echo $_SERVER["REMOTE_ADDR"];?> asks for local git status:</p>
 
 <pre>
-<?php echo shell_exec("git status 2>&1");
-?>
+<?php echo shell_exec("git status 2>&1");?>
 </pre>
 
 <p>website <a href="..">git commands</a></p>
