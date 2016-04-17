e3fa83221093351bc932bcd39fbaf5fc9a644533
Bill Ola Rasmussen <billra@gmail.com>
2016-04-17 06:41:50 +0200

    show calling ip address

--- git/pull/index.php
+++ git/pull/index.php
@@ -2,11 +2,10 @@
 <html>
 <body>
 
-<p>Pulling from github:</p>
+<p><?echo $_SERVER["REMOTE_ADDR"];?> asks for a pull from github:</p>
 
 <pre>
-<?php echo shell_exec("git pull 2>&1");
-?>
+<?php echo shell_exec("git pull 2>&1");?>
 </pre>
 
 <p>website <a href="..">git commands</a></p>
