e13e86d8e8d4f4465b6ce7335306d865f1dfb7b1
Bill Ola Rasmussen <billra@gmail.com>
2016-04-17 06:09:25 +0200

    track git operations

--- git/pull/index.php
+++ git/pull/index.php
@@ -2,11 +2,21 @@
 <html>
 <body>
 
-Pulling from github:
+<p>Pulling from github:</p>
+
 <pre>
 <?php echo shell_exec("git pull 2>&1");
 ?>
 </pre>
+
+<p>website <a href="..">git commands</a></p>
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-701785-1";
+urchinTracker();
+</script>
 
 </body>
 </html>
