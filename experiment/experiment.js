061b43ad22f3dfd397f3ae92e786da538e1380f6
Bill Ola Rasmussen <billra@gmail.com>
2016-05-05 08:49:58 +0200

    create line between balls

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -59,9 +59,65 @@ function WireEditChange(htmobj,svgobj,svgattr) {
 	};
 }
 
-window.onload=function() {
+// todo: solve code duplication
+function WireSvgToEditLine(movementContainer, svgid, svgLineid, lineend, htmXid, htmYid) {
+	// lookups called only once
+	var svgobj = document.getElementById(svgid);
+	var svgLineobj = document.getElementById(svgLineid);
+	var htmXobj = document.getElementById(htmXid);
+	var htmYobj = document.getElementById(htmYid);
+
+	// wire edit to svg
+	WireEditChange(htmXobj, svgobj, 'cx');
+	WireEditChange(htmYobj, svgobj, 'cy');
+
+	// track old positions
+	var oldX; // strategy: track mouse move offset, add to object position
+	var oldY;
+	var oldColor;
+
+	// mouse down on actual svg object
+	svgobj.onmousedown = function (evt) {
+		evt.preventDefault(); // disable browser drag and drop
+		oldX = evt.clientX;
+		oldY = evt.clientY;
+		oldColor = svgobj.style.fill;
+		svgobj.style.fill = 'green';
+		// update html
+		theName.innerHTML = svgid;
+		// move tracked from svg container so we don't get a premature onmouseleave
+		movementContainer.onmousemove = function (evt) {
+			evt.preventDefault(); // disable browser drag and drop
+			nowX = parseInt(svgobj.getAttribute('cx')) + evt.clientX - oldX;
+			nowY = parseInt(svgobj.getAttribute('cy')) + evt.clientY - oldY;
+			oldX = evt.clientX;
+			oldY = evt.clientY;
+			svgobj.setAttribute('cx', nowX);
+			svgobj.setAttribute('cy', nowY);
+			svgLineobj.setAttribute('x'+lineend, nowX);
+			svgLineobj.setAttribute('y'+lineend, nowY);
+			// update contents of edit boxes
+			htmXobj.value = nowX;
+			htmYobj.value = nowY;
+		};
+		// likewise, end move tracked from container
+		var endmove = function (evt) {
+			svgobj.style.fill = oldColor;
+			// update html
+			theName.innerHTML = 'nothing';
+			// unwire svg object events
+			movementContainer.onmousemove = null;
+			movementContainer.onmouseup = null;
+			movementContainer.onmouseleave = null;
+		};
+		movementContainer.onmouseup = endmove;
+		movementContainer.onmouseleave = endmove;
+	};
+}
+
+window.onload = function () {
 	WireSvgToEdit(svg1,'yellowCircle1','yellowCircleXposEdit1','yellowCircleYposEdit1');
 	WireSvgToEdit(svg1,'orangeCircle1','orangeCircleXposEdit1','orangeCircleYposEdit1');
-	WireSvgToEdit(svg2,'yellowCircle2','yellowCircleXposEdit2','yellowCircleYposEdit2');
-	WireSvgToEdit(svg2,'orangeCircle2','orangeCircleXposEdit2','orangeCircleYposEdit2');
+	WireSvgToEditLine(svg2,'yellowCircle2','line2','1','yellowCircleXposEdit2','yellowCircleYposEdit2');
+	WireSvgToEditLine(svg2,'orangeCircle2','line2','2','orangeCircleXposEdit2','orangeCircleYposEdit2');
 };
