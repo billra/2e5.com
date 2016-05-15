d5cd71934cd37e6d0d3c363ee5eacebb4eed913b
Bill Ola Rasmussen <billra@gmail.com>
2016-05-15 08:33:50 +0200

    log window, logging, and exception handling

--- experiment/two.js
+++ experiment/two.js
@@ -2,12 +2,13 @@ var editor;
 var dynCode;
 var result;
 var textContainer;
+var dynLog;
 
 function run_code() {
 	editor.save();
 	var code = textContainer.value;
 	var script = document.createElement('script');
-	script.innerHTML = code;
+	script.innerHTML = 'try{' + code + '}catch(e){log("Code Error:",e.message);}';
 	dynCode.innerHTML = ''; // clear previous children
 	dynCode.appendChild(script);
 }
@@ -16,9 +17,19 @@ function showResult(val) {
 	result.innerHTML = val;
 }
 
+function log(msg) {
+	var str = "";
+	for (var i = 0; i < arguments.length; i++) { // comma separated items to log
+		str += ' ' + arguments[i].toString();
+	}
+	str += "<br>";
+	dynLog.innerHTML += str;
+}
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=- main -=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 window.onload = function () {
+	dynLog = document.getElementById("consoleLog");
 	dynCode = document.getElementById("dynamicCode");
 	result = document.getElementById("divTarget");
 	textContainer = document.getElementById("editor");
